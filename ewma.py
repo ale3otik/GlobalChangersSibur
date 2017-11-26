0abe8df1a3e41c470a2f27d991bb07cd9d966ff0
sergmiller <sergey.miller@phystech.edu>
2017-11-26 05:48:59 +0300

    add andrey's code

---
+++ ewma.py
@@ -0,0 +1,47 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+import re
+import pickle
+import matplotlib.pyplot as plt
+
+
+def get_target(data, top=.05, **kwargs):
+    '''accepts dataframe of numeric, returns np.array of shape (data.shape[0], data.shape[1]) with 1s and 0s'''
+    ewma = np.array(data.ewm(**kwargs).var())
+    ewma_k_stat = []
+    for i in range(ewma.shape[1]):
+        a = ewma[:, i]
+        a = a[np.isfinite(a)]
+        k = int(top * a.shape[0])
+        ewma_k_stat.append(np.partition(a, k)[:k].max())
+    ewma_k_stat = np.array(ewma_k_stat)
+    target = np.array([[1 if np.isfinite(ewma[i, j]) and ewma[i, j] >= ewma_k_stat[j] else 0
+               for j in range(ewma.shape[1])]
+              for i in range(ewma.shape[0])])
+    return target
+
+def rolling_sum(a, n=1000) :
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret
+
+################ RUN THIS ###################
+def get_target_future(data, horizont=1000, min_anomaly=50, top=.1, **kwargs):
+    target = get_target(data, top, **kwargs)
+    dim = target.shape[1]
+    target = np.vectorize(lambda v: 1 if v >= min(dim / 2, 1) else 0)(target.sum(axis=1))
+    target = rolling_sum(target, n=horizont)
+    target_future = target
+    target_future[:-horizont] = target[horizont:]
+    target_future[-horizont:] = np.full(shape=(horizont,), fill_value=target[-1])
+    return np.vectorize(lambda s: 1 if s >= min_anomaly else 0)(target_future)
+
+
+
+#target = get_target(train[['efficiency', 'energy_cons', 'quality']].iloc[:100000], halflife=100)
+#print(target[-80:])
+# target = get_target_future(train[['efficiency', 'energy_cons', 'quality']],
+#                            horizont=1000, min_anomaly=100, top=.01,
+#                            halflife=100)
